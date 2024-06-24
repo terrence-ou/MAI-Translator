bd2ddd709a16665c338d632e88b7591f8881cc27
terrence-ou <outerrencedl@gmail.com>
2024-06-23 21:57:21 -0400

    project config updated

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,24 @@
-import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
   },
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+        "@": resolve("src/renderer/src"),
+        "@renderer": resolve("/src/renderer/src"),
+        "@shared": resolve("/src/shared"),
+        "@/hooks": resolve("/src/renderer/src/hooks"),
+        "@/components": resolve("/src/renderer/src/components"),
+      },
     },
-    plugins: [react()]
-  }
-})
+    plugins: [react()],
+  },
+});
