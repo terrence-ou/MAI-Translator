3218bc2c4ea6a296d631a684729369e3b056de5b
terrence-ou <outerrencedl@gmail.com>
2024-06-26 09:54:10 -0400

    React StrictMode restored

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,13 +1,13 @@
 import "./assets/index.css";
-// import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "./components/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <ThemeProvider defaultTheme="dark">
-    <App />
-  </ThemeProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="dark">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
