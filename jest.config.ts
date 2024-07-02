ab54877dbf6420dba41262d708c310dd4f9eade2
terrence-ou <outerrencedl@gmail.com>
2024-07-02 13:58:58 -0400

    testing environment setup updated

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,16 @@
-// import type { Config } from "jest";
 import type { JestConfigWithTsJest } from "ts-jest";
 
 const config: JestConfigWithTsJest = {
   testEnvironment: "jsdom",
   roots: ["src/renderer/src/__tests__"],
   preset: "ts-jest",
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.web.json",
-    },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.web.json",
+      },
+    ],
   },
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/renderer/src/$1",
