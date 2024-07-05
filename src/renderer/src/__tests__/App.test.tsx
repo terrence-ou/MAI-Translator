4c646f7f70eb636f73f961f8cc19c16aa78ed9e9
terrence-ou <outerrencedl@gmail.com>
2024-07-05 09:55:36 -0400

    test cases updated

--- src/renderer/src/__tests__/App.test.tsx
+++ src/renderer/src/__tests__/App.test.tsx
@@ -1,12 +1,20 @@
 import "@testing-library/dom";
 import "@testing-library/jest-dom";
 import "@/utils/window-apis.mock";
+import { act } from "react";
 import { render, screen } from "@/utils/test-utils";
 import App from "@/App";
 
+// we need to use act because we have a useEffect running when mounting the app
+const renderApp = async () => {
+  await act(async () => {
+    render(<App />);
+  });
+};
+
 describe("Testing main interface layout", () => {
   test("basic elements", async () => {
-    render(<App />);
+    await renderApp();
     const header = screen.queryByTestId("header");
     const main = screen.queryByTestId("main");
     const panelLeft = screen.queryByTestId("panel-left");
@@ -20,9 +28,9 @@ describe("Testing main interface layout", () => {
 
 describe("Testing initial texts on the interface", () => {
   test("language selector texts", async () => {
-    render(<App />);
+    await renderApp();
     expect(await screen.findByText("English")).toBeVisible();
     expect(await screen.findByText("Detect Language")).toBeVisible();
-    expect(await screen.findByText("中文")).not.toBeVisible();
+    expect(screen.queryByText("中文")).toBeNull();
   });
 });
--- src/renderer/src/__tests__/Header.test.tsx
+++ src/renderer/src/__tests__/Header.test.tsx
@@ -2,14 +2,36 @@ import "@testing-library/dom";
 import "@testing-library/jest-dom";
 import "@/utils/window-apis.mock";
 import { render, screen } from "@/utils/test-utils";
+import { fireEvent } from "@/utils/test-utils";
+// import App from "@/App";
 import Header from "@/components/header/Header";
 
 describe("Testing header", () => {
   test("check default buttons", () => {
     render(<Header />);
-    const panelButtonClose = screen.queryByTestId("button-panel-close");
-    const panelButtonOpen = screen.queryByTestId("button-panel-open");
-    expect(panelButtonClose).toBeInTheDocument();
-    expect(panelButtonOpen).not.toBeInTheDocument(); // at this point, the open button shouldn't be displayed
+    expect(getCloseBtn()).toBeInTheDocument();
+    expect(getOpenBtn()).not.toBeInTheDocument(); // at this point, the open button shouldn't be displayed
+  });
+
+  test("toggling panel button", () => {
+    render(<Header />);
+    const closeBtn = getCloseBtn();
+    expect(closeBtn).toBeInTheDocument();
+    fireEvent.click(closeBtn!); // if the previous line passed, then there must be a close button here
+    expect(getOpenBtn()).toBeInTheDocument();
+    expect(getCloseBtn()).not.toBeInTheDocument();
+
+    const openBtn = getOpenBtn();
+    fireEvent.click(openBtn!);
+    expect(getCloseBtn()).toBeInTheDocument();
+    expect(getOpenBtn()).not.toBeInTheDocument();
   });
 });
+
+// Helper functions
+const getCloseBtn = () => {
+  return screen.queryByTestId("button-panel-close");
+};
+const getOpenBtn = () => {
+  return screen.queryByTestId("button-panel-open");
+};
