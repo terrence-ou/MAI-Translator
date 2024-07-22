486d0f79e778874e0b5b90e654438c10fad912ac
terrence-ou <outerrencedl@gmail.com>
2024-07-22 14:02:03 -0400

    date month bug fixed

--- src/main/lib/files.ts
+++ src/main/lib/files.ts
@@ -43,7 +43,7 @@ export const writeHistory: WriteHistoryFn = async (content) => {
 const getTimeString = (): string => {
   const now = new Date();
   const year = now.getFullYear();
-  const month = String(now.getMonth()).padStart(2, "0");
+  const month = String(now.getMonth() + 1).padStart(2, "0");
   const day = String(now.getDate()).padStart(2, "0");
   const hour = String(now.getHours()).padStart(2, "0");
   const minutes = String(now.getMinutes()).padStart(2, "0");
